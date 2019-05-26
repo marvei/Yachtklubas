dd5132cd5c9d67d0a7fee23d0ff227c80d1d53c0
Julius <0wn3das@gmail.com>
2019-05-26 18:47:59 +0300

    Urodai nx

---
+++ FInalFinal/Yachtos/Yachtos/Database/Database.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Yachtos.Models
+{
+    public class DatabaseContext : DbContext
+    {
+           //CIa reikia rasyti tuos tokius Tables nu klases vienu zodziu.
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=Yachtos.db");
+            }
+
+    }
+}

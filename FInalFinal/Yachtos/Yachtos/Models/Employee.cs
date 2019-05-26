b1b01a6e4949f9cf98405f1b7ba267dc5fddda6a
marvei <martynas.veikutis@ktu.edu>
2019-05-27 00:00:48 +0300

    Employee index and create

--- FInalFinal/Yachtos/Yachtos/Models/Employee.cs
+++ FInalFinal/Yachtos/Yachtos/Models/Employee.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Yachtos.Database;
+using System.Web;
 
 namespace Yachtos.Models
 {
@@ -9,5 +11,80 @@ namespace Yachtos.Models
     {
         public string EmployeeId { get; set; }
         public User fk_User { get; set; }
+        
+
+        public List<Employee> GetEmployee()
+        {
+            List<Employee> Listas = new List<Employee>();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Employee)
+                {
+                    Listas.Add(new Employee
+                    {
+                        EmployeeId = this.EmployeeId,
+                        fk_User = this.fk_User
+                    }) ;
+                }
+            }
+            return Listas;
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.User)
+                {
+                    users.Add(new User
+                    {
+                        Username = item.Username,
+                        Id = item.Id
+                    });
+                }
+            }
+            return users;
+        }
+
+        public void Create()
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Employee.Add(new Employee { EmployeeId = this.EmployeeId, fk_User = this.fk_User });
+                db.SaveChanges();
+            }
+        }
+
+        public Employee GetEmployee(string id)
+        {
+            Employee emp = new Employee();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Employee)
+                {
+                    if (item.EmployeeId == id)
+                    {
+                        emp = item;
+                    }
+                }
+            }
+            return emp;
+        }
+
+        public void DeleteEmployeeById(string id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Employee)
+                {
+                    if (item.EmployeeId == id)
+                    {
+                        db.Employee.Remove(item);
+                        db.SaveChanges();
+                    }
+                }
+            }
+        }
     }
 }

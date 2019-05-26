567324f4a7793187b19380e4eabaed5ff7eaa76e
Julius <0wn3das@gmail.com>
2019-05-26 23:04:47 +0300

    Delete function works.

--- FInalFinal/Yachtos/Yachtos/Controllers/StoragesController.cs
+++ FInalFinal/Yachtos/Yachtos/Controllers/StoragesController.cs
@@ -12,142 +12,43 @@ namespace Yachtos.Controllers
 {
     public class StoragesController : Controller
     {
-        private readonly DatabaseContext _context;
-
-        public StoragesController(DatabaseContext context)
+        public IActionResult Index()
         {
-            _context = context;
+            Storage storage= new Storage();
+            return View(storage.GetStorage());
         }
-
-        // GET: Storages
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Storage.ToListAsync());
-        }
-
-        // GET: Storages/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var storage = await _context.Storage
-                .FirstOrDefaultAsync(m => m.id == id);
-            if (storage == null)
-            {
-                return NotFound();
-            }
-
-            return View(storage);
-        }
-
-        // GET: Storages/Create
         public IActionResult Create()
         {
+
             return View();
         }
-
-        // POST: Storages/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Create")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,Item,description,Image,kiekis")] Storage storage)
+        public IActionResult CreateConfirmed([Bind("Id, Item, description, Image, kiekis")] Storage storage)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(storage);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(storage);
-        }
 
-        // GET: Storages/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            storage.Create();
 
-            var storage = await _context.Storage.FindAsync(id);
-            if (storage == null)
-            {
-                return NotFound();
-            }
-            return View(storage);
-        }
-
-        // POST: Storages/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("id,Item,description,Image,kiekis")] Storage storage)
-        {
-            if (id != storage.id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(storage);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StorageExists(storage.id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(storage);
-        }
-
-        // GET: Storages/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var storage = await _context.Storage
-                .FirstOrDefaultAsync(m => m.id == id);
-            if (storage == null)
-            {
-                return NotFound();
-            }
-
-            return View(storage);
-        }
-
-        // POST: Storages/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var storage = await _context.Storage.FindAsync(id);
-            _context.Storage.Remove(storage);
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-        private bool StorageExists(int id)
+        public IActionResult Delete(int? id)
         {
-            return _context.Storage.Any(e => e.id == id);
+            int? PollId = id;
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            Storage poll = new Storage();
+            return View(poll.GetPoll(id: PollId));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            int PollId = id;
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            Storage poll = new Storage();
+            poll.DeletePollById(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- FInalFinal/Yachtos/Yachtos/Models/Storage.cs
+++ FInalFinal/Yachtos/Yachtos/Models/Storage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Yachtos.Database;
 
 namespace Yachtos.Models
 {
@@ -12,6 +13,64 @@ namespace Yachtos.Models
         public string description { get; set; }
         public string Image { get; set; }
         public int kiekis { get; set; }
-        
+
+        public List<Storage> GetStorage()
+        {
+            List<Storage> Listas = new List<Storage>();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Storage)
+                {
+                    Listas.Add(new Storage
+                    {
+                        id = item.id,
+                        Item = item.Item,
+                        description = item.description,
+                        Image = item.Image,
+                        kiekis = item.kiekis
+                    });
+                }
+            }
+            return Listas;
+        }
+        public void Create()
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Storage.Add(new Storage { id = this.id, description = this.description, Image = this.Image, kiekis = this.kiekis ,Item = this.Item});
+                db.SaveChanges();
+            }
+        }
+
+        public Storage GetPoll(int? id)
+        {
+            Storage stor = new Storage();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Storage)
+                {
+                    if(item.id == id)
+                    {
+                        stor = item;
+                    }
+                }
+            }
+            return stor;
+        }
+
+        public void DeletePollById(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                foreach(var item in db.Storage)
+                {
+                    if(item.id == id)
+                    {
+                        db.Storage.Remove(item);
+                        db.SaveChanges();
+                    }
+                }
+            }
+        }
     }
 }

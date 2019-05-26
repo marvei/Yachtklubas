0e178040387754c7432c344beaab992371962c08
Julius <0wn3das@gmail.com>
2019-05-26 21:31:01 +0300

    Belekas

---
+++ FInalFinal/Yachtos/Yachtos/Controllers/StoragesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Yachtos.Database;
+using Yachtos.Models;
+
+namespace Yachtos.Controllers
+{
+    public class StoragesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public StoragesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Storages
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Storage.ToListAsync());
+        }
+
+        // GET: Storages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storage = await _context.Storage
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (storage == null)
+            {
+                return NotFound();
+            }
+
+            return View(storage);
+        }
+
+        // GET: Storages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Storages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Item,description,Image,kiekis")] Storage storage)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(storage);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(storage);
+        }
+
+        // GET: Storages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storage = await _context.Storage.FindAsync(id);
+            if (storage == null)
+            {
+                return NotFound();
+            }
+            return View(storage);
+        }
+
+        // POST: Storages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Item,description,Image,kiekis")] Storage storage)
+        {
+            if (id != storage.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(storage);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StorageExists(storage.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(storage);
+        }
+
+        // GET: Storages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storage = await _context.Storage
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (storage == null)
+            {
+                return NotFound();
+            }
+
+            return View(storage);
+        }
+
+        // POST: Storages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var storage = await _context.Storage.FindAsync(id);
+            _context.Storage.Remove(storage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StorageExists(int id)
+        {
+            return _context.Storage.Any(e => e.id == id);
+        }
+    }
+}

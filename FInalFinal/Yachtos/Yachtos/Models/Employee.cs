d64852d046f83821a96ac46b3f0afe24b0372d67
marvei <martynas.veikutis@ktu.edu>
2019-05-27 03:18:24 +0300

    employee done

--- FInalFinal/Yachtos/Yachtos/Models/Employee.cs
+++ FInalFinal/Yachtos/Yachtos/Models/Employee.cs
@@ -26,8 +26,8 @@ namespace Yachtos.Models
                 {
                     Listas.Add(new Employee
                     {
-                        EmployeeId = this.EmployeeId,
-                        fk_User = this.fk_User
+                        EmployeeId = item.EmployeeId,
+                        UserId = item.UserId
                     }) ;
                 }
             }
@@ -51,11 +51,21 @@ namespace Yachtos.Models
             return users;
         }
 
+        public User getUserById(int id)
+        {
+            User temp = new User();
+            using (var db = new DatabaseContext())
+            {
+                temp = db.User.Find(id);
+            }
+            return temp;
+        }
+
         public void Create()
         {
             using (var db = new DatabaseContext())
             {
-                db.Employee.Add(new Employee { EmployeeId = this.EmployeeId, fk_User = this.fk_User });
+                db.Employee.Add(new Employee { EmployeeId = this.EmployeeId, UserId = this.UserId });
                 db.SaveChanges();
             }
         }

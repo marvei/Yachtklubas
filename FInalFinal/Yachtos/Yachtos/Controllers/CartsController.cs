1e4e7aab9fc184359294a705a74253a5520c1a75
marvei <martynas.veikutis@ktu.edu>
2019-05-27 09:19:37 +0300

    cart added, need auto addtocart

--- FInalFinal/Yachtos/Yachtos/Controllers/CartsController.cs
+++ FInalFinal/Yachtos/Yachtos/Controllers/CartsController.cs
@@ -12,142 +12,27 @@ namespace Yachtos.Controllers
 {
     public class CartsController : Controller
     {
-        private readonly DatabaseContext _context;
-
-        public CartsController(DatabaseContext context)
+        public IActionResult Index()
         {
-            _context = context;
+            Cart cart = new Cart();
+            return View(cart.getCarts());
         }
-
-        // GET: Carts
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Cart.ToListAsync());
-        }
-
-        // GET: Carts/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cart = await _context.Cart
-                .FirstOrDefaultAsync(m => m.CartId == id);
-            if (cart == null)
-            {
-                return NotFound();
-            }
-
-            return View(cart);
-        }
-
-        // GET: Carts/Create
         public IActionResult Create()
         {
-            return View();
+            Cart cart = new Cart();
+            return View(cart);
         }
-
-        // POST: Carts/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Create")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CartId,ItemsId")] Cart cart)
+        public IActionResult CreateConfirmed([Bind("CartId, ItemsId")] Cart cart)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cart);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cart);
-        }
+            //int c = int.Parse(Request.Form["UserId"]);
 
-        // GET: Carts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var cart = await _context.Cart.FindAsync(id);
-            if (cart == null)
-            {
-                return NotFound();
-            }
-            return View(cart);
-        }
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            cart.Create();
 
-        // POST: Carts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CartId,ItemsId")] Cart cart)
-        {
-            if (id != cart.CartId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cart);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CartExists(cart.CartId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cart);
-        }
-
-        // GET: Carts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cart = await _context.Cart
-                .FirstOrDefaultAsync(m => m.CartId == id);
-            if (cart == null)
-            {
-                return NotFound();
-            }
-
-            return View(cart);
-        }
-
-        // POST: Carts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cart = await _context.Cart.FindAsync(id);
-            _context.Cart.Remove(cart);
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CartExists(int id)
-        {
-            return _context.Cart.Any(e => e.CartId == id);
         }
     }
 }
--- FInalFinal/Yachtos/Yachtos/Models/Cart.cs
+++ FInalFinal/Yachtos/Yachtos/Models/Cart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Yachtos.Database;
 
 namespace Yachtos.Models
 {
@@ -12,5 +13,65 @@ namespace Yachtos.Models
         public List<Items> Items { get; set; }
         [ForeignKey("Id")]
         public int ItemsId { get; set; }
+
+
+        public List<Cart> getCarts()
+        {
+            List<Cart> Listas = new List<Cart>();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Cart)
+                {
+                    Listas.Add(new Cart { CartId = item.CartId, ItemsId = item.ItemsId });
+                }
+            }
+            return Listas;
+        }
+
+        public List<Items> GetItems()
+        {
+            List<Items> Listas = new List<Items>();
+            using (var db = new DatabaseContext())
+            {
+                foreach (var item in db.Items)
+                {
+                    Listas.Add(new Items
+                    {
+                        StorageId = item.StorageId,
+                        price = item.price
+                    });
+                }
+            }
+            return Listas;
+        }
+
+        public Storage getItemInfo(int id)
+        {
+            Storage temp = new Storage();
+            using (var db = new DatabaseContext())
+            {
+                temp = db.Storage.Find(id);
+            }
+            return temp;
+        }
+
+        public Items getItemPrice(int id)
+        {
+            Items temp = new Items();
+            using (var db = new DatabaseContext())
+            {
+                temp = db.Items.Find(id);
+            }
+            return temp;
+        }
+
+        public void Create()
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Cart.Add(new Cart { CartId = this.CartId, ItemsId = this.ItemsId });
+                db.SaveChanges();
+            }
+        }
     }
 }

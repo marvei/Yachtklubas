52b74daed4dcf7193ef193a04433be731a267d76
Minimaksas <benas.klimasauskas@ktu.edu>
2019-05-27 04:21:09 +0300

    items

--- FInalFinal/Yachtos/Yachtos/Controllers/ItemsController.cs
+++ FInalFinal/Yachtos/Yachtos/Controllers/ItemsController.cs
@@ -16,13 +16,42 @@ namespace Yachtos.Controllers
         // GET: Items
         public IActionResult Index()
         {
-
-            return View();
+            Items items = new Items();
+            return View(items.GetItem());
         }
         public IActionResult Create()
         {
             Items items = new Items();
             return View(items);
+        }
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateConfirmed([Bind("id, price, StorageId")] Items items)
+        {
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            items.Create();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            int? PollId = id;
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            Items poll = new Items();
+            return View(poll.GetItem(id: PollId));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            int PollId = id;
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+            Items poll = new Items();
+            poll.DeleteItemById(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
